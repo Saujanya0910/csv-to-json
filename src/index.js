6f61a535f1022a9de84824cae2ead5f59c61df70
Saujanya Pandey <saujanya.pandey2000@gmail.com>
2025-04-19 17:25:13 +0530

    chore: initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+process.on('SIGTERM', async () => {
+  process.exit(0);
+});
